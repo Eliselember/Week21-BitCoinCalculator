4ee9091cfe263f205b7fd94a92eb02dd358f623e
Eliselember <89841862+Eliselember@users.noreply.github.com>
2022-01-24 14:02:53 +0200

    Update BitCoinRate.cs

--- BitCoinCalc/BitCoinRate.cs
+++ BitCoinCalc/BitCoinRate.cs
@@ -12,9 +12,21 @@ namespace BitCoinCalc
     public class bpi
     {
         public EUR EUR { get; set; }
+        public USD USD { get; set; }
+        public GBP GBP { get; set; }
     }
 
     public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+    public class USD
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+    public class GBP
     {
         public string code { get; set; }
         public float rate_float { get; set; }
